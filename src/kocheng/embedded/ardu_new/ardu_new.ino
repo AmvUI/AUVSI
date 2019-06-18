8e866fecc0701b77d806bae968a86aa68fad0695
lp02781 <lp02781@gmail.com>
2019-06-18 19:31:45 +0700

    18 June 2019

--- src/kocheng/embedded/ardu_new/ardu_new.ino
+++ src/kocheng/embedded/ardu_new/ardu_new.ino
@@ -1,26 +1,27 @@
 int pwm_value1;
 int pwm_value2;
-int middle_pwm = 1600;
+int middle_pwm = 1400;
 
 //pin for killswitch
-int kill_1=3;
-int kill_2=4;
-int kill_3=5;
-int kill_4=6;
-int remote_kill_1=9;
-int remote_kill_2=10;
+int kill_1=22;
+int kill_2=24;
+int kill_3=26;
+int kill_4=28;
+int remote_kill_1=6; //receive 5
+int remote_kill_2=7; // receive 6
+int button = A12;
 
 //pin lampu
-int remote_pin = A3;
-int remote_pin_2 = A4;
+int remote_pin = 4; //receive 4
 
-int led_red     = A0;
-int led_yellow  = A1;
-int led_green   = A2;
+int led_red     = 46;
+int led_yellow  = 48;
+int led_green   = 50;
+
+int button_value;
 
 int pwm_remote;
 int pwm_remote_2;
-int middle_analog = 130;
 
 void setup() {
   pinMode (kill_1,OUTPUT);
@@ -29,13 +30,13 @@ void setup() {
   pinMode (kill_4,OUTPUT);
   pinMode (remote_kill_1,INPUT);
   pinMode (remote_kill_2,INPUT);
+  pinMode (remote_pin, INPUT);
+  pinMode (button, INPUT);
   
   pinMode(led_red, OUTPUT);
   pinMode(led_green, OUTPUT);
   pinMode(led_yellow, OUTPUT);
 
-  pinMode(remote_pin, INPUT);
-  pinMode(remote_pin_2, INPUT);
   
   Serial.begin (9600);
 }
@@ -43,11 +44,16 @@ void setup() {
 void loop() {
   pwm_value1 = pulseIn(remote_kill_1, HIGH);
   pwm_value2 = pulseIn(remote_kill_2, HIGH);
+  pwm_remote = pulseIn(remote_pin, HIGH);
+  button_value = analogRead(button);
+  
   Serial.println(pwm_value1);
   Serial.println(pwm_value2);
+  Serial.println(pwm_remote);
+  Serial.println(button_value);
   Serial.println("    ");
 
-  if (pwm_value1>middle_pwm)
+  if (pwm_value1<middle_pwm)
   {
     digitalWrite (kill_1,HIGH);
     digitalWrite (kill_2,HIGH);
@@ -59,7 +65,7 @@ void loop() {
   }
   delay (100);
   
-  if (pwm_value2>middle_pwm)
+  if (pwm_value2<middle_pwm)
   {
     digitalWrite (kill_3,HIGH);
     digitalWrite (kill_4,HIGH);
@@ -70,17 +76,15 @@ void loop() {
     digitalWrite (kill_4,LOW);
   }
 
-  pwm_remote = analogRead(remote_pin);
-  pwm_remote_2 = pulseIn(remote_pin_2, HIGH);
   delay (100);
 
-    if(pwm_remote>middle_analog){//motor
+  if(pwm_value1<middle_pwm || button_value < 900){//motor
     digitalWrite(led_red, HIGH);
     digitalWrite(led_yellow, LOW);
     digitalWrite(led_green, LOW);    
   }
   else {
-    if(pwm_remote_2>middle_pwm){//auto
+    if(pwm_remote<middle_pwm){//auto
       digitalWrite(led_red, LOW);
       digitalWrite(led_yellow, LOW);
       digitalWrite(led_green, HIGH);
